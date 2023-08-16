de6593573c5319b97bcf7949f41b10782085b7fe
Imran Hossain <imranhossainsakil22@.com>
2023-08-16 10:32:52 +0600

    utilities file changes

--- js/utilities.js
+++ js/utilities.js
@@ -6,22 +6,15 @@ function getValueFromInputFiled (inputFiledId){
     inputFiledElement.value = '';
     return value;
 }
-
 // Funtion to get value from a element by ID
-
 function getValueFromElement (elementId){
     const element = document.getElementById(elementId);
     const elementString = element.innerText;
     const value = parseFloat(elementString);
     return value;
 }
-
-
 // Set value to element
-
 function setElementValue (id, newValue){
     const element = document.getElementById(id);
     element.innerText = newValue;
 }
-
-
